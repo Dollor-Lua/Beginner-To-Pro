51737cd21f60eba42badf797e382e8bd70432322
DollorLua <66220545+Dollor-Lua@users.noreply.github.com>
2020-09-13 00:03:16 -0600

    Create episode 1.lua

---
+++ episode 1.lua
@@ -0,0 +1,69 @@
+-- Beginner to pro, Episode 1
+-- printing, simple strings
+
+-- print has some basic functionality
+-- outputs the specified string into output
+-- A string is anything that is between "", '', or [[]]
+-- for example: "This is a string", 'This is a string', [[This is a string]]
+
+-- Make sure you have your output window open.
+
+-- Also the double dashes im using means a comment, something that wont effect your code.
+-- this means that if I press run this script will do nothing since this is all comments.
+
+-- You can also write comments between --[[]] like that
+-- ex:
+
+--[[
+	this is a block comment
+]]
+
+-- lets start with printing. This script will be in a github repository (in the description)
+-- along with other scripts done in more videos.
+
+-- Also note that my colors will be different than yours!
+-- The text colors don't matter, just follow this tutorial as intended.
+
+print("Hello, World!") -- this should say Hello, World! in the output!
+
+-- make sure your output window is open so you can see the printed text.
+
+-- As I said earlier, you can write strings in different ways
+
+print('Hello, World!')
+
+-- Here is the block string:
+
+print([[Hello, World!]])
+
+-- this should print hello, world! 3 times since they all do the same thing.
+-- As you saw, it worked!
+
+-- I did say they are block strings which means
+
+print([[This 
+Is 
+Multi 
+Lined]])
+
+-- You can have multi lined strings!
+
+-- there are different ways of printing too! More will be explained in more advanced
+-- videos. Here are some more versions of print:
+
+warn("This is a warning")
+
+-- it works the same way, it supports all 3 versions of strings!
+
+-- the last way of printing is error()
+
+error("This is an error!")
+
+-- I recommend using normal quotes for strings since you will use single quotes and brackets
+-- more often.
+
+-- when I press play you will see orange text and red text
+
+-- this is where the tutorial script ends
+-- make sure ot like and subscribe, stay tuned for more videos in this series!
+-- github repository in description of the video!
